49a7714b7b9c8075c664c9592979faf24870e095
Fabulousman23 <andrew.makiev@gmail.com>
2021-09-29 22:12:47 -0400

    create list of items

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-let 
+let pokemonList = [
+  { 
+    name: 'Onix',
+    height: 8.8,
+    types: ['petrified', 'crawling']
+  },
+  { name: 'Tauros',
+    height: 1.4,
+    types: ['horned', ' prancing']
+  },
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    types: ['electric', 'walking']
+  }
+];
+console.log(pokemonList);
+
