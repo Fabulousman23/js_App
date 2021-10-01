ee832d9c14098533090c6538408a5ee99eaa3c88
Fabulousman23 <andrew.makiev@gmail.com>
2021-10-01 15:32:19 -0400

    progress part 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Create list of items */
 let pokemonList = [
   { name: 'Onix',
     height: 8.8,
@@ -8,11 +9,18 @@ let pokemonList = [
     height: 1.4,
     types: ['horned', ' prancing']
   },
-  
+
   { name: 'Pikachu',
     height: 0.4,
     types: ['electric', 'walking']
   }
 ];
 console.log(pokemonList);
+
+// loop iterates over each items
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(
+    pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + ")"
+  );
+}
 
