5074e5ba3ec2b1cb6db72e22b82c11ab74c40107
Fabulousman23 <andrew.makiev@gmail.com>
2021-10-02 14:10:44 -0400

    some updates to style and loops in js

--- js/script.js
+++ js/script.js
@@ -17,10 +17,24 @@ let pokemonList = [
 ];
 console.log(pokemonList);
 
+
 // loop iterates over each items
 for (let i = 0; i < pokemonList.length; i++) {
   document.write(
-    pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + ")"
-  );
-}
+    pokemonList[i].name + ' ' + '(height:' + ' ' + pokemonList[i].height + ')'
+  )
+};
+
+
+// adding conditional to a loop
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 4) {
+    console.log(pokemonList[i].name + ' (height' + pokemonList[i].height + ')'+ ' OMG! He\'s huge')
+  } else if (pokemonList[i].height < 4 && pokemonList[i].height > 1) {
+    console.log(pokemonList[i].name + ' (height' + pokemonList[i].height + ')')
+  } else {
+    console.log(pokemonList[i].name + ' (height' + pokemonList[i].height + ')'+ ' He\'s such a baby, so cute')
+  }
+};
+
 
