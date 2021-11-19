ae8bac06fc96341cab970a9a9697ad4008ec2ea5
Fabulousman23 <andrew.makiev@gmail.com>
2021-11-19 14:17:06 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,43 @@
 /* Create list of items */
-let pokemonList = [
-  { name: 'Onix',
-    height: 8.8,
-    types: ['petrified', 'crawling']
-  },
+  let pokemonList = [
+    { name: 'Onix',
+      height: 8.8,
+      types: ['petrified', 'crawling']
+    },
 
-  { name: 'Tauros',
-    height: 1.4,
-    types: ['horned', ' prancing']
-  },
+    { name: 'Tauros',
+      height: 1.4,
+      types: ['horned', ' prancing']
+    },
 
-  { name: 'Pikachu',
-    height: 0.4,
-    types: ['electric', 'walking']
-  }
-];
+    { name: 'Pikachu',
+      height: 0.4,
+      types: ['electric', 'walking']
+    }
+  ];
 console.log(pokemonList);
 
 
+
+
+//  loop iterates using forEach()
+function printPokemon2(user) {
+  console.log(user.name);
+}
+pokemonList.forEach(printPokemon2);
+
+
+
 // loop iterates over each items
+
 function printPokemon(list){
 for (let i = 0; i < list.length; i++) {
   document.write(
    '<p>' + pokemonList[i].name + ' ' + '(height:' + ' ' + pokemonList[i].height + ')<br>'+ '</p>'
   )
+  };
 };
-};
+
 printPokemon(pokemonList);
 
 // adding conditional to a loop
@@ -39,4 +51,6 @@ for (let i = 0; i < pokemonList.length; i++) {
     console.log(pokemonList[i].name + addText + pokemonList[i].height + ')'+ ' He\'s such a baby, so cute')
   }
 };
+
+
 
