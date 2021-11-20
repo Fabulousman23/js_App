98e486ba7bb6095547ae32f8de8b6c3c7488b684
Fabulousman23 <andrew.makiev@gmail.com>
2021-11-20 14:29:34 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 /* Create list of items */
-  let pokemonList = [
+   let pokemonProject = (function () {
+   let pokemonList = [
     { name: 'Onix',
       height: 8.8,
-      types: ['petrified', 'crawling']
+      types: ['petrified', ' crawling']
     },
 
     { name: 'Tauros',
@@ -12,45 +13,65 @@
 
     { name: 'Pikachu',
       height: 0.4,
-      types: ['electric', 'walking']
+      types: ['electric', ' walking']
     }
   ];
-console.log(pokemonList);
 
-
-
-
-//  loop iterates using forEach()
-function printPokemon2(user) {
-  console.log(user.name);
-}
-pokemonList.forEach(printPokemon2);
-
-
-
-// loop iterates over each items
-
-function printPokemon(list){
-for (let i = 0; i < list.length; i++) {
-  document.write(
-   '<p>' + pokemonList[i].name + ' ' + '(height:' + ' ' + pokemonList[i].height + ')<br>'+ '</p>'
-  )
-  };
-};
-
-printPokemon(pokemonList);
-
-// adding conditional to a loop
-let addText = ' (height: ';
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 4) {
-    console.log(pokemonList[i].name + addText + pokemonList[i].height + ')'+ ' OMG! He\'s huge')
-  } else if (pokemonList[i].height < 4 && pokemonList[i].height > 1) {
-    console.log(pokemonList[i].name + addText + pokemonList[i].height + ')')
-  } else {
-    console.log(pokemonList[i].name + addText + pokemonList[i].height + ')'+ ' He\'s such a baby, so cute')
+  function getAll() {
+    return pokemonList;
   }
-};
+
+  function add (pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+
+  })();
+
+  console.log(pokemonProject.getAll());
+  
+  pokemonProject.getAll().forEach(function(pokemon) {
+    document.write('<p class="pokedex-style">' + pokemon.name + '<br>' + 'Height: ' + pokemon.height + 'm<br>' + 'Types: ' + pokemon.types + '</p>');
+});
+
+
+
+
+
+// //  loop iterates using forEach()
+// pokemonList.forEach(function(pokemon) {
+//   console.log(pokemon.name + ' is ' + pokemon.height + ' '+ pokemon.types);
+// });
+
+
+
+// // loop iterates over each items
+
+// function printPokemon(list){
+// for (let i = 0; i < list.length; i++) {
+//   document.write(
+//    '<p>' + pokemonList[i].name + ' ' + '(height:' + ' ' + pokemonList[i].height + ')<br>'+ '</p>'
+//   )
+//   };
+// };
+
+// printPokemon(pokemonList);
+
+// // adding conditional to a loop
+// let addText = ' (height: ';
+// for (let i = 0; i < pokemonList.length; i++) {
+//   if (pokemonList[i].height > 4) {
+//     console.log(pokemonList[i].name + addText + pokemonList[i].height + ')'+ ' OMG! He\'s huge')
+//   } else if (pokemonList[i].height < 4 && pokemonList[i].height > 1) {
+//     console.log(pokemonList[i].name + addText + pokemonList[i].height + ')')
+//   } else {
+//     console.log(pokemonList[i].name + addText + pokemonList[i].height + ')'+ ' He\'s such a baby, so cute')
+//   }
+// };
 
 
 
