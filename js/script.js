fe0dbd78606246313a6507464a45448360f6a594
Fabulousman23 <andrew.makiev@gmail.com>
2022-01-11 16:53:15 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let modalContainer = document.querySelector('#modal-container');
 
   function add(pokemon) {
     if (
@@ -60,17 +61,111 @@ let pokemonRepository = (function () {
 
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function () {
-      console.log(item);
+      const text = `height: ${item.height} \n
+        type: ${item.name}`
+        showModal(item.name, text, item.imageUrl);
+    });
+  }
+  
+  function showModal(title, text, img) {
+    // Clear all existing modal content
+    modalContainer.innerHTML = '';
+    
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+    
+    // Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+    
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+    
+    let imgElement = document.createElement('img');
+    imgElement.src=img;
+
+    let contentElement = document.createElement('div');
+    contentElement.innerText = text;
+    
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(imgElement);
+    modalContainer.appendChild(modal);
+    
+    modalContainer.classList.add('is-visible');
+  }
+  let dialogPromiseReject;
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+
+    if (dialogPromiseReject) {
+      dialogPromiseReject();
+      dialogPromiseReject = null;
+    }
+  }
+
+  function showDialog(title, text) {
+    showModal(title, text);
+
+    let modalContainer = document.querySelector('#modal-container');
+
+    let modal = modalContainer.querySelector('.modal');
+
+    let confirmButton = document.createElement('button');
+
+    confirmButton.classList.add('modal-confirm');
+    confirmButton.innerText = 'confirm'; 
+
+    let cancelButton = document.createElement('button');
+    
+    cancelButton.classList.add('modal-cancel');
+    cancelButton.innerText = 'cancel';
+
+    modal.appendChild(confirmButton);
+    modal.appendChild(cancelButton);
+
+    confirmButton.focus();
+
+    return new Promise ((resolve, reject)=> {
+       cancelButton.addEventListener('click', hideModal);
+       confirmButton.addEventListener('click',()=> {
+        dialogPromiseReject = null; 
+        hideModal();
+        resolve();
+      })
+      dialogPromiseReject = reject;
     });
   }
 
+
+  
+  window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
+      hideModal();
+    }
+  });
+  
+  modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal container,
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
+  
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
     loadList: loadList,
     loadDetails: loadDetails,
-    showDetails: showDetails
+    showDetails: showDetails,
   };
 })();
 
@@ -80,8 +175,6 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
-
-
 
 
 
